e073cbb4776b66fb1d1743b6bb02ef8a9bb6e4ab
dynii <dyniiyeyo@naver.com>
2025-12-07 15:00:31 +0900

    Spring AI - Practice 8

--- SpringAI/src/main/java/com/dynii/springai/domain/openai/service/ChatTools.java
+++ SpringAI/src/main/java/com/dynii/springai/domain/openai/service/ChatTools.java
@@ -6,6 +6,6 @@ import org.springframework.ai.tool.annotation.Tool;
 public class ChatTools {
     @Tool(description = "User personal information : name, age, address, phone, etc")
     public UserResponseDTO getUserInfoTool() {
-        return new UserResponseDTO("김지훈", 15L, "서울특별시 종로구 청와대로 1", "010-0000-0000", "03048");
+        return new UserResponseDTO("홍길동", 20L, "서울특별시 강남구 강남대로 1", "010-0000-0000", "00000");
     }
 }
